4f0c19cbb98ecb8736a3d73c466a97da1c00daee
unknown <58160208@go.buu.ac.th>
2017-12-16 02:10:28 +0700

    three

--- contact/index.js
+++ contact/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 let contactList = [
-    {id: 0, name: 'paolo', email: 'paolo@google.com', phone: '005-456-7890', url: 'www.google.com', notes: 'Winter is coming.'},
-    {id: 1, name: 'Sven', email: 'Sven@winterfell.com', phone: '087-456-7890', url: 'www.google.com', notes: 'Reluctant to pay iron price.'},
+    {id: 0, name: 'dduilnwza007', email: 'dduilnwza007@google.com', phone: '005-420-7890', url: 'www.google.com', notes: 'Winter is coming.'},
+    {id: 1, name: 'invoker', email: 'injoker007@winterfell.com', phone: '087-456-7890', url: 'www.google.com', notes: 'Reluctant to pay iron price.'},
     {id: 2, name: 'Samwell Tarly', email: 'starly@castleblack.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Loyal brother of the watch.'},
     {id: 3, name: 'anurak Snow', email: 'jsnow@castleblack.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Knows nothing.'},
     {id: 4, name: 'Arya Stark', email: 'waterdancer@winterfell.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Has a list of names.'},
@@ -13,7 +13,7 @@ let contactList = [
     {id: 8, name: 'Hodor', email: 'hodor@hodor.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Hodor? Hodor... Hodor!'},
     {id: 9, name: 'Margaery Tyrell', email: 'mtyrell@highgarden.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Keeper of kings.'},
     {id: 10, name: 'Brienne osefaw', email: 'oathkeeper@gmail.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Do not cross her.'},
-    {id: 11, name: 'Petyr Baelish', email: 'petyr@baelishindustries.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Do not trust anyone.'},
+    {id: 11, name: 'guccigang', email: 'guccigang@baelishindustries.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Do not trust anyone.'},
   ];
 
 
@@ -30,4 +30,16 @@ router.get('/contacts', (req, res) =>{
 	else{
 		res.json(contactList)
 	}
+})
+
+router.get('/contacts/:id', (req, res) => {
+    let id = req.params.id
+    res.json(contactList[id])
+})
+
+router.post('/contacts', (req, res) => {
+    let newContact = req.body
+    newContact.id = contactList.length
+    contactList.push(newContact)
+    res.status(201).json(newContact)
 })
